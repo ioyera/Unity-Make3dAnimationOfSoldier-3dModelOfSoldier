6418c4fab655a0d78ea56c897efa29dc936b3fa6
Yerassyl Duisenbi <erasyl.d97@gmail.com>
2018-04-30 12:43:12 +0600

    first commit

---
+++ Assets/Animation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animation : MonoBehaviour {
+
+	Animator anim;
+	[SerializeField]
+	private float speed =5;
+	private float x;
+	private float z;
+	public Image GameOverImage; 
+
+	public Rigidbody MyRigidBody{ get;set; }
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+		MyRigidBody = GetComponent<Rigidbody>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat("moveH",Input.GetAxis("Horizontal"));
+		anim.SetFloat("moveV",Input.GetAxis("Vertical"));
+		x = Input.GetAxis("Horizontal") * speed*  Time.deltaTime; 
+		z = Input.GetAxis("Vertical") * speed * Time.deltaTime; 
+		transform.Translate(x, 0, z);
+	}
+	void OnCollisionExit(Collision other){
+		if (other.gameObject.tag == "dest") {
+			GameOverImage.gameObject.SetActive (true);
+			Debug.Log("kerghliuehrlgiu");
+		}
+	}
+}
---
+++ Assets/ded.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ded : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject Ragdoll;
+	public GameObject dead;
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "dest") {
+			Player.SetActive (false);
+			Ragdoll.SetActive (true);
+			Instantiate(Ragdoll, transform.position, transform.rotation);
+			Destroy (dead);
+		}
+	}
+
+}
